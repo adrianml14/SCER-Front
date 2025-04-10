b7c2e7afef31ca9643b10c4648550e32bae7bf49
adrianml14 <adrianml14@educastur.es>
2025-04-10 21:38:07 +0200

    mercado con colores + menu comprar

--- src/app/game/mercado/mercado.component.ts
+++ src/app/game/mercado/mercado.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RallyService } from '../../services/rally.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-mercado',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, MatTabsModule],
+  imports: [CommonModule, HttpClientModule, MatTabsModule, MatButtonModule],
   templateUrl: './mercado.component.html',
   styleUrl: './mercado.component.css',
 })
@@ -18,9 +20,32 @@ export class MercadoComponent implements OnInit {
   copilotos: any[] = [];
   coches: any[] = [];
 
+  elementoSeleccionado: any = null;
+  tipoSeleccionado: 'piloto' | 'copiloto' | 'coche' | null = null;
+
   ngOnInit(): void {
     this.rallyService.getPilotos().subscribe(data => this.pilotos = data);
     this.rallyService.getCopilotos().subscribe(data => this.copilotos = data);
     this.rallyService.getCoches().subscribe(data => this.coches = data);
+  }
+
+  abrirPopup(tipo: 'piloto' | 'copiloto' | 'coche', item: any) {
+    this.tipoSeleccionado = tipo;
+    this.elementoSeleccionado = item;
+  }
+
+  cerrarPopup() {
+    this.elementoSeleccionado = null;
+    this.tipoSeleccionado = null;
+  }
+
+  contratar() {
+    console.log('Contratando:', this.elementoSeleccionado);
+    this.cerrarPopup();
+  }
+
+  comprar() {
+    console.log('Comprando:', this.elementoSeleccionado);
+    this.cerrarPopup();
   }
 }
