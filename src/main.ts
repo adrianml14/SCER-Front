48a5162244b7f4c35fac305f069dc6b802b82a75
adrianml14 <adrianml14@educastur.es>
2025-04-10 19:24:25 +0200

    na

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http'; // 👈 IMPORTA ESTO
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(), // ✅ ESTA ES LA CLAVE
+    provideHttpClient(),
     ...appConfig.providers,
   ],
 }).catch((err) => console.error(err));
